f1af234fc9c309885e844598280d8e9ccb37119e
Dekxisosta <rroquxii@gmail.com>
2026-09-09 06:00:21 +0800

    feat: add powershell configuration

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+winfetch
+
+oh-my-posh init pwsh --config ~/dekxi.omp.json | Invoke-Expression
+
+
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
